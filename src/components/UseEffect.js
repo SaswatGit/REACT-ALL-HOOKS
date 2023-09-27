74aecf4e52e7befef57b4553de9a719e8d4c1111
Saswat <gitsaswat@gmail.com>
2023-09-27 07:30:38 +0530

    Added UseEffect

--- src/components/UseEffect.js
+++ src/components/UseEffect.js
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import './css/UseEffect.css';
 
 const UseEffect = () => {
+
+  const [count, setCount] = useState(0);
+
+  useEffect(()=>{
+    setInterval(() => {
+      setCount(count + 1);
+    }, 1000);
+  });
+
   return (
-    <div>UseEffect</div>
+    <div className='useeffect'>
+      <h1>{count} sec</h1>
+    </div>
   )
 }
 
